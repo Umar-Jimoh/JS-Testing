039d0ba85d02d6f17a105a1ecbd6e32d078432e3
umar-Jimoh <umarjimoh@hotmail.com>
2024-04-14 16:46:38 +0800

    Test calculateDiscount in core module

--- test/core.test.js
+++ test/core.test.js
@@ -1,5 +1,5 @@
 import { describe, it,expect } from "vitest";
-import { getCoupons } from "../src/core";
+import { calculateDiscount, getCoupons } from "../src/core";
 
 describe("getCoupons", () => {
     it('should return an array of coupons', () => {
@@ -25,5 +25,28 @@ describe("getCoupons", () => {
         expect(coupon.discount).toBeGreaterThan(0)
         expect(coupon.discount).toBeLessThan(1)
       })
+    })
+})
+
+describe('calculateDiscount', () => {
+    it('should return discounted price ifgiven valid', () => {
+        expect(calculateDiscount(10, 'SAVE10')).toBe(9)
+        expect(calculateDiscount(10, 'SAVE20')).toBe(8)
+    })
+
+    it('should handle non-numeic price', () => {
+        expect(calculateDiscount('10', 'SAVE10')).toMatch(/invalid/i)
+    })
+
+    it('should handle negative price', () => {
+        expect(calculateDiscount(-10, 'SAVE10')).toMatch(/invalid/i)
+    })
+
+    it('should handle non-string discount code', () => {
+        expect(calculateDiscount(10, 10)).toMatch(/invalid/i)
+    })
+
+    it('should handle invalid discount code', () => {
+        expect(calculateDiscount('10', 'INVALID')).toMatch(/invalid/i)
     })
 })
