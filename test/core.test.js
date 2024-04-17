8c168f8ecd0c7206babc4cc52dc695dbd240148e
umar-Jimoh <umarjimoh@hotmail.com>
2024-04-18 05:54:53 +0800

    Test Stack Class in core js

--- test/core.test.js
+++ test/core.test.js
@@ -8,6 +8,7 @@ import {
   afterAll,
 } from 'vitest'
 import {
+  Stack,
   calculateDiscount,
   canDrive,
   fetchData,
@@ -183,8 +184,73 @@ describe('Test suite', () => {
   afterEach(() => {
     console.log('afterEach called')
   })
-  
+
   it('test case 1', () => {})
 
   it('test case 1', () => {})
+})
+
+describe('stack', () => {
+  let stack
+
+  beforeEach(() => {
+    stack = new Stack()
+  })
+
+  it('push should add an item to the stack', () => {
+    stack.push(1)
+
+    expect(stack.size()).toBe(1)
+  })
+
+  it('pop should remove and return an item in the stack', () => {
+    stack.push(1)
+    stack.push(2)
+
+    expect(stack.pop()).toBe(2)
+    expect(stack.size()).toBe(1)
+  })
+
+  it('pop should throw an Error if stack is empty', () => {
+    expect(() => stack.pop()).toThrow(/empty/i)
+  })
+
+  it('peek should return the top item in the stack without removing it', () => {
+    stack.push(1)
+    stack.push(2)
+
+    const peeekedItem = stack.peek()
+
+    expect(peeekedItem).toBe(2)
+    expect(stack.size()).toBe(2)
+  })
+
+  it('peek should throw an Error if stack is empty', () => {
+    expect(() => stack.peek()).toThrow(/empty/i)
+  })
+
+  it('isEmpty should return true if stack is empty', () => {
+    expect(stack.isEmpty()).toBe(true)
+  })
+
+  it('isEmpty should return false if stack is not empty', () => {
+    stack.push(2)
+    
+    expect(stack.isEmpty()).toBe(false)
+  })
+
+  it('size should return the number of items in  the  stack', () => {
+    stack.push(1)
+    stack.push(2)
+
+    expect(stack.size()).toBe(2)
+  })
+
+  it('clear should remove all items from the stack', () => {
+    stack.push(2)
+    stack.push(1)
+    
+    expect(stack.clear()).toBe()
+    expect(stack.size()).toBe(0)
+  })
 })
