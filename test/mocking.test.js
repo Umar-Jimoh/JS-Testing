f122c25e59dcba15998e608c8a7e881ec0150d70
umar-Jimoh <umarjimoh@hotmail.com>
2024-04-19 16:05:14 +0800

    Test a mock module

--- test/mocking.test.js
+++ test/mocking.test.js
@@ -1,4 +1,8 @@
 import { vi, it, expect, describe } from 'vitest'
+import { getPriceInCurrency } from '../src/mocking'
+import { getExchangeRate } from '../src/libs/currency'
+
+vi.mock('../src/libs/currency')
 
 describe('test suite', () => {
     it('test case', () => {
@@ -19,5 +23,15 @@ describe('test suite', () => {
 
         expect(sendText).toHaveBeenCalled('message')
         expect(result).toBe('ok')
+    })
+})
+
+describe('getPiceInCurrency', () => {
+    it('should return price in target currency', () => {
+        vi.mocked(getExchangeRate).mockReturnValue(1.5)
+        
+        const price = getPriceInCurrency(10, 'AUD')
+
+        expect(price).toBe(15)
     })
 })
