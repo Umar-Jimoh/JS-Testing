84a2f7eae2ffed9a6a62c671cb34542886911727
umar-Jimoh <umarjimoh@hotmail.com>
2024-04-21 20:19:20 +0800

    Spying on a function

--- test/mocking.test.js
+++ test/mocking.test.js
@@ -2,6 +2,7 @@ import { vi, it, expect, describe } from 'vitest'
 import {
   getPriceInCurrency,
   getShippingInfo,
+  login,
   renderPage,
   signUp,
   submitOrder,
@@ -11,6 +12,7 @@ import { getShippingQuote } from '../src/libs/shipping'
 import { trackPageView } from '../src/libs/analytics'
 import { charge } from '../src/libs/payment'
 import { sendEmail } from '../src/libs/email'
+import security from '../src/libs/security'
 
 vi.mock('../src/libs/currency')
 vi.mock('../src/libs/shipping')
@@ -141,5 +143,17 @@ describe('signUp', () => {
     const arg = vi.mocked(sendEmail).mock.calls[0]
     expect(arg[0]).toBe(email)
     expect(arg[1]).toMatch(/welcome/i)
+  })
+})
+
+describe('login', () => {
+  it('should email the one-time login code', async () => {
+    const email = 'name@domain.com'
+    const spy = vi.spyOn(security, 'generateCode')
+
+    await login(email)
+
+    const securityCode = spy.mock.results[0].value.toString()
+    expect(sendEmail).toHaveBeenCalledWith(email, securityCode)
   })
 })
